a34516cba62afc34a2718fe501a6184e79fbf47d
seeren <consultant@seeren.fr>
2020-12-25 14:04:09 +0100

    Simplify

--- src/app/courses/course-list/course-list.component.js
+++ src/app/courses/course-list/course-list.component.js
@@ -6,7 +6,7 @@ import template from './course-list.component.html';
 import { StickyEventService } from '../../shared/services/events/sticky.event.service';
 import { SpinnerComponent } from '../../shared/components/spinner/spinner.component';
 import { RetryComponent } from '../../shared/components/retry/retry.component';
-import { CourseListService } from '../shared/services/couse-list.service';
+import { CourseListService } from './couse-list.service';
 import { AbortError } from '../../shared/errors/abort.error';
 
 /**
@@ -19,42 +19,7 @@ export class CourseListComponent extends Component {
      */
     constructor() {
         super({ selector: 'app-course-list', template });
-
         this.courseList = null;
-
-        /**
-         * @event
-         */
-        this.onAbort = () => {
-            CourseListService.abort();
-            RouterService.detach(this.onAbort);
-        };
-
-        /**
-         * @event
-         */
-        this.onResize = () => {
-            this.onDestroy();
-            this.onUpdate();
-        };
-
-        /**
-         * @event
-         * @param {Event} event
-         */
-        this.onScroll = (event) => StickyEventService.onscroll(
-            event.target,
-            document.querySelector(`${this.selector} .mdl-layout__header`),
-            // @ts-ignore
-            document.querySelector(`${this.selector} header`).offsetHeight - 36,
-        );
-    }
-
-    /**
-     * @emits
-     */
-    onInit() {
-        RouterService.attach(this.onAbort);
     }
 
     /**
@@ -62,9 +27,25 @@ export class CourseListComponent extends Component {
      */
     onUpdate() {
         if (this.courseList) {
-            document.querySelector('main.mdl-layout__content').addEventListener('scroll', this.onScroll);
-            window.addEventListener('resize', this.onResize);
+            document.querySelector('main.mdl-layout__content').addEventListener(
+                'scroll',
+                this.onScroll = (event) => StickyEventService.onscroll(
+                    event.target,
+                    document.querySelector(`${this.selector} .mdl-layout__header`),
+                    // @ts-ignore
+                    document.querySelector(`${this.selector} header`).offsetHeight - 36,
+                ),
+            );
+            window.addEventListener('resize', this.onResize = () => {
+                this.onDestroy();
+                this.onUpdate();
+            });
         } else if (!this.components.length) {
+            RouterService.attach(this.onAbort = () => {
+                CourseListService.abort();
+                RouterService.detach(this.onAbort);
+                delete this.onAbort;
+            });
             this.showAll();
         }
     }
@@ -90,17 +71,16 @@ export class CourseListComponent extends Component {
         };
         this.attach(spinner);
         this.update();
-        CourseListService
-            .get()
+        CourseListService.get()
             .then(() => {
-                this.courseList = CourseListService.courseList;
                 this.detach(spinner);
+                this.courseList = CourseListService.courseList;
                 this.update();
             })
             .catch((e) => {
+                this.detach(spinner);
                 if (!(e instanceof AbortError)) {
                     this.attach(retry);
-                    this.detach(spinner);
                     this.update();
                 }
             });
