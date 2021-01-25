dbe1103588aa61d9ae3678cb7cc6dd81d50d198b
seeren <consultant@seeren.fr>
2021-01-25 18:56:15 +0100

    Create stepper

--- src/app/courses/chapter/chapter.component.js
+++ src/app/courses/chapter/chapter.component.js
@@ -1,7 +1,15 @@
-import { Component } from 'appable';
+import { Component, RouterComponent } from 'appable';
+
+import { SpinnerComponent } from '../../shared/components/spinner/spinner.component';
+import { SpinnerService } from '../../shared/components/spinner/spinner.service';
+import { AbortError } from '../../shared/errors/abort.error';
+import { AbortService } from '../../shared/services/abort.service';
+import { MdlService } from '../../shared/services/mdl.service';
+import { ColorService } from '../shared/services/color.service';
 
 // @ts-ignore
 import template from './chapter.component.html';
+import { ChapterService } from './chapter.service';
 
 /**
  * @type {ChapterComponent}
@@ -12,7 +20,71 @@ export class ChapterComponent extends Component {
      * @constructor
      */
     constructor() {
-        super({ selector: 'chapter', template });
+        super({ selector: 'app-chapter', template });
+    }
+
+    /**
+     * @emits
+     */
+    onInit() {
+        this.color = null;
+        this.chapter = null;
+        this.length = null;
+        this.section = RouterComponent.get('section');
+        // this.section = 'Scrum';
+    }
+
+    /**
+     * @emits
+     */
+    onUpdate() {
+        if (this.chapter) {
+            MdlService.upgradeOne(`${this.selector} .mdl-js-ripple-effect`);
+        } else if (!this.components.length) {
+            this.onAbort = AbortService.add(ChapterService);
+            this.show(RouterComponent.get('name'), RouterComponent.get('chapter'));
+            // this.show('Agile', 'Présentation des principales approches Agiles');
+        }
+    }
+
+    /**
+     * @emits
+     */
+    onDestroy() {
+        this.components.forEach((component) => this.detach(component));
+    }
+
+    /**
+     * @param {String} name
+     * @param {String} chapter
+     */
+    show(name, chapter) {
+        const spinner = new SpinnerComponent();
+        const retry = SpinnerService.start(this, spinner, () => this.show(name, chapter));
+        ChapterService.get(name, chapter)
+            .then((data) => {
+                this.chapter = data;
+                this.length = data.document.querySelectorAll('h2').length;
+                this.color = ColorService.get(name);
+            })
+            .catch((error) => error instanceof AbortError || this.attach(retry))
+            .finally(() => {
+                AbortService.remove(this.onAbort);
+                if (this.components.length || this.chapter) {
+                    this.detach(spinner);
+                    this.update();
+                }
+            });
+    }
+
+    /**
+     * @event
+     */
+    terminate() {
+        ChapterService.terminate(this.chapter);
+        RouterComponent.navigate('formation', {
+            name: RouterComponent.get('name'),
+        });
     }
 
 }
---
+++ src/app/courses/chapter/chapter.service.js
@@ -0,0 +1,40 @@
+import { Service } from 'appable';
+
+import { NotFoundError } from '../../shared/errors/not-found.error';
+import { CourseListService } from '../course-list/couse-list.service';
+import { CourseService } from '../course/course.service';
+import { Wiki } from '../shared/models/wiki.model';
+
+/**
+ * @type {ChapterService}
+ */
+// @ts-ignore
+export const ChapterService = new class extends Service {
+
+    /**
+     * @param {String} name
+     * @param {String} chapter
+     * @returns {Promise<Wiki>}
+     */
+    get(name, chapter) {
+        return new Promise((resolve, reject) => {
+            CourseService.get(name)
+                .then((course) => {
+                    const wiki = course.wikiList.find(
+                        (readme) => chapter === readme.document.querySelector('h1').innerText,
+                    );
+                    return wiki ? resolve(wiki) : reject(new NotFoundError());
+                })
+                .catch((error) => reject(error));
+        });
+    }
+
+    /**
+     * @param {Wiki} wiki
+     */
+    terminate(wiki) {
+        wiki.checked = true;
+        CourseListService.save();
+    }
+
+}();
