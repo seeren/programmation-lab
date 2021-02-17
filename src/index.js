ab7c66bb80f6c3d206c438d4fb00059e6062558b
seeren <consultant@seeren.fr>
2021-02-17 16:38:50 +0100

    Use main path

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import { CourseComponent } from './app/courses/course/course.component';
 import { FavoriteListComponent } from './app/favorites/favorite-list/favorite-list.component';
 import { ChapterComponent } from './app/courses/chapter/chapter.component';
 
-((run) => (window.cordova
-    ? window.document.addEventListener('deviceready', run)
-    : window.addEventListener('load', run))
+((main) => (window.cordova
+    ? window.document.addEventListener('deviceready', main)
+    : window.addEventListener('load', main))
 )(() => RouterComponent
-    .add('/formations', 'formations', CourseListComponent)
-    .add('/formations/:name', 'formation', CourseComponent)
-    .add('/formations/:name/:chapter/:section', 'chapitre', ChapterComponent)
-    .add('/seeren', 'main', MainComponent)
+    .add('/', 'main', MainComponent)
+    .add('/formations', 'courses', CourseListComponent)
+    .add('/formations/:course', 'course', CourseComponent)
+    .add('/formations/:course/:chapter/:section', 'chapter', ChapterComponent)
     .add('/favoris', 'favorites', FavoriteListComponent)
     .run(new AppComponent()));
