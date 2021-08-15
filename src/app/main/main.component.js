c6fcbb5d86d6d7c8cab75a5e1a3b5c5946176898
seeren <consultant@seeren.fr>
2021-08-15 23:47:38 +0200

    Refactor MainComponent

--- src/app/main/main.component.js
+++ src/app/main/main.component.js
@@ -1,48 +1,26 @@
 import { Component, RouterComponent } from 'appable';
 
-// @ts-ignore
 import template from './main.component.html';
 
-import { LastService } from '../courses/shared/services/last.service';
 import { MdlService } from '../shared/services/mdl.service';
+import { CourseService } from '../courses/shared/services/course.service';
 
-/**
- * @type {MainComponent}
- */
 export class MainComponent extends Component {
 
-    /**
-     * @constructor
-     */
+    course = CourseService.get();
+
     constructor() {
-        super({ selector: 'app-main', template });
+        super('app-main', template);
     }
 
-    /**
-     * @emits
-     */
-    onInit() {
-        this.last = LastService.last;
-    }
-
-    /**
-     * @emits
-     */
     onUpdate() {
         MdlService.upgradeOne(`${this.selector} .mdl-button`);
     }
 
-    /**
-     * @event
-     * @param {String} course
-     */
-    show(course) {
-        RouterComponent.navigate('course', { course });
+    show(courseName) {
+        RouterComponent.navigate('course', { course: courseName });
     }
 
-    /**
-     * @event
-     */
     showAll() {
         RouterComponent.navigate('courses');
     }
