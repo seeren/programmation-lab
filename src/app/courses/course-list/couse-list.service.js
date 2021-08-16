4bff98e0cb7113a447ea915925f3badf1ae5230e
seeren <consultant@seeren.fr>
2021-08-16 21:13:59 +0200

    Use fetch

--- src/app/courses/course-list/couse-list.service.js
+++ src/app/courses/course-list/couse-list.service.js
@@ -3,61 +3,36 @@ import { environment } from '../../../../environment/environment.prod';
 import { HttpClientService } from '../../shared/services/http-client.service';
 import { RateError } from '../../shared/errors/rate.error';
 import { LocalStorageService } from '../../shared/services/local-storage.service';
-import { Course } from '../course/cours.model';
 import { CourseListBuilder } from './course-list.builder';
+import { NetworkError } from '../../shared/errors/network.error';
+import { AbortError } from '../../shared/errors/abort.error';
+import { AbortService } from '../../shared/services/abort.service';
 
-/**
- * @type {CourseListService}
- */
-// @ts-ignore
 export const CourseListService = new class extends HttpClientService {
 
-    /**
-     * @constructor
-     */
-    constructor() {
-        super();
+    #courseList = LocalStorageService.get(environment.storage.courseList) || [];
 
-        /**
-         * @type {Array<Course>}
-         */
-        this.courseList = LocalStorageService.get(environment.storage.courseList) || [];
-
-        /**
-         * @type {CourseListBuilder}
-         */
-        this.builder = new CourseListBuilder();
+    async fetch() {
+        let repositoryList;
+        try {
+            AbortService.add(CourseListService);
+            repositoryList = await (fetch(`https://api.github.com/users/${environment.organisation}/repos`, {
+                signal: this.controller.signal,
+                headers: { Authorization: `token ${environment.token}` },
+            }).then((response) => response.json()));
+        } catch (error) {
+            throw error instanceof DOMException ? new AbortError() : new NetworkError();
+        }
+        if (!Array.isArray(repositoryList)) {
+            throw new RateError();
+        }
+        this.#courseList = new CourseListBuilder().build(repositoryList, this.#courseList);
+        LocalStorageService.set(environment.storage.courseList, this.#courseList, 86400);
+        return this.#courseList;
     }
 
-    /**
-     * @returns {Promise<Array<Course>>}
-     */
-    get() {
-        return new Promise((resolve, reject) => {
-            if (this.courseList.length && !this.courseList.find((course) => !course.description)) {
-                return resolve(this.courseList);
-            }
-            this.request(reject);
-            this.xhr.open('GET', `https://api.github.com/users/${environment.organisation}/repos`);
-            this.xhr.setRequestHeader('Authorization', `token ${environment.token}`);
-            this.xhr.onload = () => {
-                const repositoryList = JSON.parse(this.xhr.response);
-                if (!Array.isArray(repositoryList)) {
-                    return reject(new RateError());
-                }
-                this.courseList = this.builder.build(repositoryList, this.courseList);
-                this.save();
-                return resolve(this.courseList);
-            };
-            this.xhr.send();
-        });
-    }
-
-    /**
-     * @returns {void}
-     */
-    save() {
-        LocalStorageService.set(environment.storage.courseList, this.courseList, 86400);
+    get courseList() {
+        return this.#courseList;
     }
 
 }();
