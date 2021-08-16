e36f0fa8e08e3e6cecc25cb9ee1b1d910cdf3236
seeren <consultant@seeren.fr>
2021-08-16 13:00:57 +0200

    Use AbortController

--- src/app/shared/services/http-client.service.js
+++ src/app/shared/services/http-client.service.js
@@ -1,32 +1,15 @@
 import { Service } from 'appable';
 
-import { AbortError } from '../errors/abort.error';
-
-/**
- * @type {HttpClientService}
- */
 export class HttpClientService extends Service {
 
-    /**
-     * @param {Function} reject
-     * @returns {XMLHttpRequest}
-     */
-    request(reject) {
-        this.aborted = false;
-        this.xhr = new XMLHttpRequest();
-        this.xhr.onerror = (e) => reject(e);
-        this.xhr.onabort = () => reject(new AbortError());
-        return this.xhr;
+    #controller;
+
+    get controller() {
+        return this.#controller = new AbortController();
     }
 
-    /**
-     * @returns {void}
-     */
     abort() {
-        this.aborted = true;
-        if (this.xhr && 0 < this.xhr.readyState) {
-            this.xhr.abort();
-        }
+        this.#controller.abort();
     }
 
 }
