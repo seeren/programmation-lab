0e2e425689c6205bc8bfdc5553e5dbbac4e43a0b
seeren <consultant@seeren.fr>
2021-08-19 20:49:17 +0200

    Cache data

--- src/app/courses/course/course.service.js
+++ src/app/courses/course/course.service.js
@@ -6,101 +6,48 @@ import { RateError } from '../../shared/errors/rate.error';
 import { CourseListService } from '../course-list/couse-list.service';
 import { WikiListService } from '../shared/services/wiki-list.service';
 import { CourseBuilder } from './course.builder';
-import { Course } from './cours.model';
-import { Wiki } from '../shared/models/wiki.model';
+import { AbortService } from '../../shared/services/abort.service';
+import { AbortError } from '../../shared/errors/abort.error';
+import { NetworkError } from '../../shared/errors/network.error';
 
-/**
- * @type {CourseService}
- */
-// @ts-ignore
 export const CourseService = new class extends HttpClientService {
 
-    constructor() {
-        super();
-
-        /**
-         * @type {CourseBuilder}
-         */
-        this.builder = new CourseBuilder();
+    async fetch(courseName) {
+        let readme;
+        const builder = new CourseBuilder();
+        const existingCourse = CourseListService.courseList
+            .find((item) => item.name === courseName);
+        if (existingCourse && existingCourse.readme && existingCourse.wikiList) {
+            if (!(existingCourse.readme.document instanceof DocumentFragment)) {
+                builder.decorate(existingCourse);
+            }
+            return existingCourse;
+        }
+        try {
+            AbortService.add(CourseService);
+            readme = await (fetch(`https://api.github.com/repos/${environment.organisation}/${courseName}/readme`, {
+                signal: this.controller.signal,
+                headers: { Authorization: `token ${environment.token}` },
+            }).then((response) => response.json()));
+        } catch (error) {
+            throw error instanceof DOMException ? new AbortError() : new NetworkError();
+        }
+        if ('Not Found' === readme.message) {
+            throw new NotFoundError(`Repo "${environment.organisation}/${courseName}" not found`);
+        } else if (!readme.content) {
+            throw new RateError();
+        }
+        const course = builder.build(CourseListService.courseList, readme, courseName);
+        course.wikiList = await WikiListService.fetch(courseName);
+        CourseListService.save();
+        builder.decorate(course);
+        return course;
     }
 
-    /**
-     * @param {Course} course
-     * @returns {Number}
-     */
     toPercent(course) {
         let checked = 0;
         course.wikiList.forEach((wiki) => checked += wiki.checked ? 1 : 0);
         return Math.round((checked / (course.wikiList.length - 1)) * 100);
-    }
-
-    /**
-     * @param {String} name
-     * @returns {Promise<Course>}
-     */
-    get(name) {
-        return new Promise((resolve, reject) => {
-            const course = CourseListService.courseList.find(
-
-                /**
-                 * @param {Course} item
-                 */
-                (item) => item.name === name,
-            );
-            if (course && course.readme && course.wikiList) {
-                if (!(course.readme.document instanceof DocumentFragment)) {
-                    this.builder.decorate(course);
-                }
-                return resolve(course);
-            }
-            this.request(reject);
-            this.xhr.open('GET', `https://api.github.com/repos/${environment.organisation}/${name}/readme`);
-            this.xhr.setRequestHeader('Authorization', `token ${environment.token}`);
-            this.xhr.onload = () => this.onload(resolve, reject, name);
-            this.xhr.send();
-        });
-    }
-
-    /**
-     * @param {Function} resolve
-     * @param {Function} reject
-     * @param {String} name
-     */
-    onload(resolve, reject, name) {
-        const readme = JSON.parse(this.xhr.response);
-        if (404 === this.xhr.status) {
-            return reject(new NotFoundError(`Repo "${environment.organisation}/${name}" not found`));
-        }
-        if (!readme.content) {
-            return reject(new RateError());
-        }
-        const course = this.builder.build(CourseListService.courseList, readme, name);
-        WikiListService.get(name).then(
-
-            /**
-             * @param {Wiki[]} wikiList
-             */
-            (wikiList) => {
-                course.wikiList = wikiList;
-                CourseListService.save();
-                this.builder.decorate(course);
-                resolve(course);
-            },
-        ).catch(
-
-            /**
-             * @param {Error} e
-             */
-            (e) => reject(e),
-        );
-    }
-
-    /**
-     * @returns {Boolean}
-     */
-    abort() {
-        super.abort();
-        return WikiListService.abort();
     }
 
 }();
