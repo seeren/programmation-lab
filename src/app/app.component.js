a4e8f2c7ca1be64409d6f0a5446b56764ce03f05
seeren <consultant@seeren.fr>
2021-08-19 16:48:58 +0200

    Refactor AppComponent

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,32 +1,16 @@
 import { Component } from 'appable';
 
-// @ts-ignore
 import template from './app.component.html';
 
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 import { MdlService } from './shared/services/mdl.service';
 
-/**
- * @type {AppComponent}
- */
 export class AppComponent extends Component {
 
-    /**
-     * @constructor
-     */
     constructor() {
-        super({
-            selector: 'app',
-            template,
-            components: [
-                new NavigationComponent(),
-            ],
-        });
+        super('app', template, [new NavigationComponent()]);
     }
 
-    /**
-     * @emits
-     */
     onUpdate() {
         MdlService.upgrade();
     }
