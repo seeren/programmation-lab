aca818d1dcfc78b596fb196cb1f9890fde901fab
seeren <consultant@seeren.fr>
2021-08-19 16:48:43 +0200

    Add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,8 @@ module.exports = {
         'no-return-assign': 0,
         'class-methods-use-this': 0,
         'consistent-return': 0,
+        'no-restricted-syntax': 0,
+        'no-await-in-loop': 0,
     },
     parser: 'babel-eslint',
 };
