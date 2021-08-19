675c346522e53fab088dc92b8d97f375a55226b3
seeren <consultant@seeren.fr>
2021-08-19 20:48:32 +0200

    Refactor ChapterService

--- src/app/courses/chapter/chapter.service.js
+++ src/app/courses/chapter/chapter.service.js
@@ -2,49 +2,19 @@ import { Service } from 'appable';
 
 import { NotFoundError } from '../../shared/errors/not-found.error';
 import { CourseListService } from '../course-list/couse-list.service';
-import { Course } from '../course/cours.model';
 import { CourseService } from '../course/course.service';
-import { Wiki } from '../shared/models/wiki.model';
 
-/**
- * @type {ChapterService}
- */
-// @ts-ignore
 export const ChapterService = new class extends Service {
 
-    /**
-     * @param {String} name
-     * @param {String} chapter
-     * @returns {Promise<Wiki>}
-     */
-    get(name, chapter) {
-        return new Promise((resolve, reject) => {
-            CourseService.get(name)
-                .then(
-
-                    /**
-                     * @param {Course} course
-                     */
-                    (course) => {
-                        const wiki = course.wikiList.find(
-                            (readme) => chapter === readme.document.querySelector('h1').innerText,
-                        );
-                        return wiki ? resolve(wiki) : reject(new NotFoundError());
-                    },
-                )
-                .catch(
-
-                    /**
-                     * @param {Error} error
-                     */
-                    (error) => reject(error),
-                );
-        });
+    async fetch(courseName, chapterName) {
+        const course = await CourseService.fetch(courseName);
+        const wiki = course.wikiList.find((readme) => chapterName === readme.document.querySelector('h1').innerText);
+        if (!wiki) {
+            throw new NotFoundError();
+        }
+        return wiki;
     }
 
-    /**
-     * @param {Wiki} wiki
-     */
     terminate(wiki) {
         wiki.checked = true;
         CourseListService.save();
