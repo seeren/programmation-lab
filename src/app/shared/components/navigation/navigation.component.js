3935df2cca7e64a20cad558a3308620d57c9aaa2
seeren <consultant@seeren.fr>
2021-08-20 15:49:23 +0200

    Refactor NavigationComponent

--- src/app/shared/components/navigation/navigation.component.js
+++ src/app/shared/components/navigation/navigation.component.js
@@ -1,6 +1,5 @@
-import { Component, RouterComponent, RouterService } from 'appable';
+import { Component, RouterComponent, StateService } from 'appable';
 
-// @ts-ignore
 import template from './navigation.component.html';
 
 import { MdlService } from '../../services/mdl.service';
@@ -8,60 +7,80 @@ import { ScrollService } from '../../services/scroll.service';
 import { FavoriteListService } from '../../../favorites/favorite-list/favorite-list.service';
 import { ChapterService } from '../../../courses/chapter/chapter.service';
 
-/**
- * @type {NavigationComponent}
- */
 export class NavigationComponent extends Component {
 
-    /**
-     * @constructor
-     */
+    routeName;
+
+    #onScroll;
+
     constructor() {
-        super({ selector: 'app-navigation', template });
-        RouterService.attach(() => this.onNavigate());
-        ChapterService.attach(() => this.upgradeFavorite());
+        super('app-navigation', template);
+        StateService.attach(() => this.#onStateChange());
+        ChapterService.attach(() => this.#onChapterChange());
     }
 
-    /**
-     * @emits
-     */
     onUpdate() {
         MdlService
             .downGrade(`${this.selector}.mdl-layout`)
             .upgradeOne(`${this.selector}.mdl-layout`)
             .upgradeOne(`${this.selector} .mdl-snackbar`)
             .upgradeAll(`${this.selector} .mdl-layout__tab-ripple-container`);
-        this.onScroll = ScrollService.add(`${this.selector} .mdl-layout__header`, 0);
+        this.#onScroll = ScrollService.add(`${this.selector} .mdl-layout__header`, 0);
         ScrollService.top();
     }
 
-    /**
-     * @emits
-     */
     onDestroy() {
         const element = document.querySelector(this.selector);
         const parent = element.parentNode;
         parent.parentNode.insertBefore(element, parent);
         parent.parentNode.removeChild(parent);
-        ScrollService.remove(this.onScroll);
+        ScrollService.remove(this.#onScroll);
     }
 
-    /**
-     * @emits
-     */
-    onNavigate() {
-        const state = RouterService.get();
-        this.route = state[`${'name'}`];
-        switch (state[`${'name'}`]) {
-        case 'courses': this.title = 'Thématiques';
+    onNavigateBack() {
+        RouterComponent.back();
+    }
+
+    onNavigate(routeName) {
+        RouterComponent.navigate(routeName);
+    }
+
+    onToogleFavorite(event) {
+        const state = StateService.get();
+        const favorite = FavoriteListService.find(state.param);
+        event.preventDefault();
+        event.stopImmediatePropagation();
+        const data = { };
+        if (favorite) {
+            data.message = 'Extrait supprimé';
+            FavoriteListService.remove(favorite);
+        } else {
+            data.message = 'Extrait ajouté';
+            FavoriteListService.add(state.param);
+        }
+        document.querySelector(`${this.selector} .mdl-snackbar`).MaterialSnackbar.showSnackbar(data);
+
+        this.#onChapterChange();
+    }
+
+    #onStateChange() {
+        const state = StateService.get();
+        this.routeName = state.name;
+        switch (this.routeName) {
+        case 'courses':
+            this.title = 'Thématiques';
             break;
-        case 'course': this.title = state[`${'param'}`].course;
+        case 'course':
+            this.title = decodeURI(state.param.course);
             break;
-        case 'chapter': this.title = state[`${'param'}`].chapter;
+        case 'chapter':
+            this.title = decodeURI(state.param.chapter);
             break;
-        case 'favorites': this.title = 'Extraits';
+        case 'favorites':
+            this.title = 'Extraits';
             break;
-        default: this.title = 'Programmation Lab';
+        default:
+            this.title = 'Programmation Lab';
         }
         if (window.document.querySelector('main.mdl-layout__content')) {
             this.onDestroy();
@@ -69,54 +88,11 @@ export class NavigationComponent extends Component {
         }
     }
 
-    /**
-     * @event
-     * @param {String} name
-     */
-    navigate(name) {
-        RouterComponent.navigate(name);
-    }
-
-    /**
-     * @param {MouseEvent} event
-     */
-    toogleFavorite(event) {
-        const state = RouterService.get();
-        const favorite = FavoriteListService.find(state[`${'param'}`]);
-        if (event) {
-            event.preventDefault();
-            event.stopImmediatePropagation();
-            const data = { };
-            if (favorite) {
-                data.message = 'Extrait supprimé';
-                FavoriteListService.remove(favorite);
-            } else {
-                data.message = 'Extrait ajouté';
-                FavoriteListService.add(FavoriteListService.clone(state[`${'param'}`]));
-            }
-            // @ts-ignore
-            document.querySelector(`${this.selector} .mdl-snackbar`).MaterialSnackbar.showSnackbar(data);
-        }
-        this.upgradeFavorite();
-    }
-
-    /**
-     * @event
-     */
-    upgradeFavorite() {
+    #onChapterChange() {
         const tab = window.document.querySelector(`${this.selector} .mdl-layout__tab.chapter`);
-        if (FavoriteListService.find(RouterService.get()[`${'param'}`])) {
-            tab.classList.add('is-active');
-        } else {
-            tab.classList.remove('is-active');
-        }
-    }
-
-    /**
-     * @event
-     */
-    back() {
-        RouterComponent.back();
+        return FavoriteListService.find(StateService.get().param)
+            ? tab.classList.add('is-active')
+            : tab.classList.remove('is-active');
     }
 
 }
