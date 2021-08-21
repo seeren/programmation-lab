768bd2e907cabe9f6168f204dd3a121a9e66762d
seeren <consultant@seeren.fr>
2021-08-22 00:07:35 +0200

    Allow empty block

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
         'consistent-return': 0,
         'no-restricted-syntax': 0,
         'no-await-in-loop': 0,
+        'no-empty': 0,
     },
     parser: 'babel-eslint',
 };
