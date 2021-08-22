492594d2d0ff67ce6f059ca16cc202238c6f731a
seeren <consultant@seeren.fr>
2021-08-22 17:23:03 +0200

    Remove public path

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            publicPath: './',
+            publicPath: 'production' !== process.env.NODE_ENV ? '/' : '',
         }),
     ],
     devServer: {
