4d686f8f1876726aa67568b9f89e2359a539184d
seeren <consultant@seeren.fr>
2021-08-22 16:26:43 +0200

    Set public path empty

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            publicPath: '/',
+            publicPath: '',
         }),
     ],
     devServer: {
