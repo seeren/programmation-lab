17afcd4b16f94dab74322caba62a9029f04a40cc
seeren <consultant@seeren.fr>
2021-11-08 11:13:08 +0100

    Fix android sort

--- src/app/courses/course-list/course-list.filter.js
+++ src/app/courses/course-list/course-list.filter.js
@@ -9,13 +9,28 @@ export class CourseListFilter {
     filter(courseList, type = CourseListFilter.DATE) {
         switch (type) {
         case CourseListFilter.NAME:
-            return courseList.sort((a, b) => a.name > b.name);
+            return courseList.sort((a, b) => {
+                if (a.name === b.name) {
+                    return 0;
+                }
+                return a.name > b.name ? 1 : -1;
+            });
         case CourseListFilter.STARS:
-            return courseList.sort((a, b) => a.stargazers < b.stargazers);
+            return courseList.sort((a, b) => {
+                if (a.stargazers === b.stargazers) {
+                    return 0;
+                }
+                return a.stargazers < b.stargazers ? 1 : -1;
+            });
         default:
-            return courseList.sort(
-                (a, b) => (new Date(b.updated)).valueOf() - (new Date(a.updated)).valueOf(),
-            );
+            return courseList.sort((a, b) => {
+                const dateA = (new Date(a.updated)).valueOf();
+                const dateB = (new Date(b.updated)).valueOf();
+                if (dateB === dateA) {
+                    return 0;
+                }
+                return dateB > dateA ? 1 : -1;
+            });
         }
     }
 
